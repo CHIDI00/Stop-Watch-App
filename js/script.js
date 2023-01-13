e410ca1743332b5e73739f4917e5b3e7de257e21
chidigit config --global user.name chidi <chidimathayas@gmail.com>
2023-01-13 23:49:29 +0100

    Eleventh commit

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function timer() {
 
     let hrs = Math.floor(seconds / 3600)
     let mins = Math.floor((seconds - (hrs * 3600)) / 60)
-    let secs = seconds % 60
+    let secs = seconds % 60 
 
     if (hrs < 10) hrs = '0' + hrs
     if (mins < 10) mins = '0' + mins
