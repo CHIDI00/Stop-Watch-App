0862bf2f93edaefc0bb4eedd7a01a51ba641fb3f
chidigit config --global user.name chidi <chidimathayas@gmail.com>
2023-01-13 20:56:17 +0100

    Tenth commit

--- js/script.js
+++ js/script.js
@@ -1,39 +1,59 @@
-const hourEl = document.getElementById('watch-container hour')
-const minutesEl = document.getElementById('minutes')
-const secondsEl = document.getElementById('seconds')
-// const milisecondEl = document.getElementById('milisecond')
+const time_El = document.querySelector('.time')
+
 
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 
 startBtn.addEventListener('click', start)
 
-let seconds = 3800;
+let seconds = 7400;
 let interval = null;
 
-function startBtnClick() {
-    seconds ++
+function timer() {
+    seconds++
 
     let hrs = Math.floor(seconds / 3600)
     let mins = Math.floor((seconds - (hrs * 3600)) / 60)
     let secs = seconds % 60
 
-    if (hrs < 10) hrs = '0' + hrs;
-    if (mins < 10) mins = '0' + mins;
-    if (secs < 10) secs = '0' + secs;
+    if (hrs < 10) hrs = '0' + hrs
+    if (mins < 10) mins = '0' + mins
+    if (secs < 10) secs = '0' + secs
 
-    hourEl.innerText = `${hrs}:${mins}:${secs}`
-    // minutesEl.innerText = ``
-    // secondsEl.innerText = `:`
-    // milisecondEl.innerHTML = `${ms}:`
+    time_El.innerText = `${hrs}:${mins}:${secs}`
 }
 
-function start() {
-    if (interval) {
-        return
-    }
-    interval = setInterval(timer, 1000)
+timer()
+
+const time_El = document.querySelector('.time')
+const start_El = document.querySelector('.start')
+const stop_El = document.querySelector('.stop')
+
+let seconds = 7400;
+let interval = null;
+
+function timer() {
+    seconds++
+
+    let hrs = Math.floor(seconds / 3600)
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60)
+    let secs = seconds % 60
+
+    if (hrs < 10) hrs = '0' + hrs
+    if (mins < 10) mins = '0' + mins
+    if (secs < 10) secs = '0' + secs
+
+    time_El.innerText = `${hrs}:${mins}:${secs}`
 }
+
+timer()
+
+// function start() {
+//     if (interval) {
+//         return
+//     }
+//     interval = setInterval(timer, 1000)
+// }
 
 
 
